878aca06d17753f67993c48af75533bc753ab331
OneAikone <aleksander.wiland10@gmail.com>
2026-02-28 22:37:57 +0100

    Big update
    Added phases: pre-launch, ascent, descent, landed, as well as phase change detection and phase changes, such as enabling the atomizer or buzzer; Added buzzer and valve pins 
    
    Still needs SD implementation (should be trivial), valve implementation, LoRa, and some fine-tuning to ensure optimality

---
+++ canSoftware.ino
@@ -0,0 +1,275 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
+#include <Adafruit_BME280.h> // Multi-sensor (pressure, humidity, smth)
+#include <Adafruit_Sensor.h> // All adafruit
+#include "Adafruit_TSL2591.h" // Light intensity
+#include <DFRobot_BMI160.h> // Accelerometer
+// connect SCL to I2C Clock
+// connect SDA to I2C Data
+// connect Vin to 3.3-5V DC
+// connect GROUND to common ground
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+#define buzzerPin 0
+#define valvePin 0
+
+DFRobot_BMI160 bmi160; // Accelerometer
+const int8_t i2c_addr = 0x69; // I2C Address
+
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // Light intensity; pass in a number for the sensor identifier (for your use later)
+Adafruit_BME280 bme; // use I2C interface
+Adafruit_Sensor *bme_temp = bme.getTemperatureSensor();
+Adafruit_Sensor *bme_pressure = bme.getPressureSensor();
+Adafruit_Sensor *bme_humidity = bme.getHumiditySensor();
+
+// Flight stages; 0 -> pre-flight; 1 -> Ascent (atomizer enables); 2 -> Descent (valve opens, atomizer disables); 3 -> Landed (Valve closes, buzzer activates)
+int mode = 0;
+
+// For descent detection using altitude measurement
+int highest_point = 0;
+int altitude = 0;
+
+bool multisensorWorks = true;
+bool accelerometerWorks = true;
+
+// Counter for accelerometer
+int temporaryCounter = 0;
+
+int16_t accelGyro[6]={0,0,0,0,0,0};
+
+std::pair<uint16_t, uint16_t> lightReadings = {0,0};
+
+
+void dispLightSensorDetails(void)
+{
+
+  sensor_t sensor;
+  tsl.getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Sensor:       ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:   ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:    ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:    ")); Serial.print(sensor.max_value); Serial.println(F(" lux"));
+  Serial.print  (F("Min Value:    ")); Serial.print(sensor.min_value); Serial.println(F(" lux"));
+  Serial.print  (F("Resolution:   ")); Serial.print(sensor.resolution, 4); Serial.println(F(" lux"));  
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+  delay(500);
+}
+
+void configureLightSensor(void)
+{
+  // You can change the gain on the fly, to adapt to brighter/dimmer light situations
+  //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
+  tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
+  //tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
+  // Changing the integration time gives you a longer time over which to sense light
+  // longer timelines are slower, but are good in very low light situtations!
+  //tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
+  /* Display the gain and integration time for reference sake */  
+  Serial.println(F("------------------------------------"));
+  Serial.print  (F("Gain:         "));
+  tsl2591Gain_t gain = tsl.getGain();
+  switch(gain)
+  {
+    case TSL2591_GAIN_LOW:
+      Serial.println(F("1x (Low)"));
+      break;
+    case TSL2591_GAIN_MED:
+      Serial.println(F("25x (Medium)"));
+      break;
+    case TSL2591_GAIN_HIGH:
+      Serial.println(F("428x (High)"));
+      break;
+    case TSL2591_GAIN_MAX:
+      Serial.println(F("9876x (Max)"));
+      break;
+  }
+  Serial.print  (F("Timing:       "));
+  Serial.print((tsl.getTiming() + 1) * 100, DEC);
+  Serial.println(F(" ms"));
+  Serial.println(F("------------------------------------"));
+  Serial.println(F(""));
+}
+
+std::pair<uint16_t, uint16_t> readLightIntensity(void)
+{
+  // More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
+  // That way you can do whatever math and comparisons you want!
+  uint32_t lum = tsl.getFullLuminosity();
+  uint16_t ir, full;
+  ir = lum >> 16;
+  full = lum & 0xFFFF;
+  //Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
+  Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
+  Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  "));
+  //Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  "));
+  Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
+  return {full,ir};
+}
+
+void setup(void)
+{
+  Serial.begin(9600);
+
+  pinMode(A5, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(valvePin, OUTPUT);
+
+  // Light setup
+  if (tsl.begin()) {
+    Serial.println(F("Found a TSL2591 sensor"));
+  }
+  else {
+    Serial.println(F("No TSL2591 Light intensity sensor found ... check your wiring?"));
+  }
+  configureLightSensor();
+  dispLightSensorDetails();
+
+  // Multi-sensor setup
+  if (!bme.begin(0x76)) {
+    Serial.println(F("Could not find a valid BME280 mult-sensor, check wiring!"));
+  }
+  else {
+    Serial.println(F("Found a BME280 sensor"));
+  }
+  bme_temp->printSensorDetails();
+  bme_pressure->printSensorDetails();
+  bme_humidity->printSensorDetails();
+
+  // Accelerometer setup
+  if (bmi160.I2cInit(i2c_addr) != BMI160_OK){
+    Serial.println(F("Failed to initialise Accelerometer, check wiring ig gng"));
+  }
+
+}
+
+// ****************************
+// IMPORTANT! REMEMBER TO TEST WHAT THE OUTPUT LOOKS LIKE WHEN THE SENSORS ARE BROKEN!
+// ****************************
+void loop(void)
+{
+  delay(1000);
+
+  // Multi sensor
+  sensors_event_t temp_event, pressure_event, humidity_event;
+
+  // [REQUIRES SENSOR FUNCTIONALITY DETECTION]
+  if(mode != 3) {
+    // TEST FOR SENSORS WORKING CORRECTLY HERE
+    // INCREMENT / RESET temporaryCounter
+    bme_temp->getEvent(&temp_event);
+    bme_pressure->getEvent(&pressure_event);
+    bme_humidity->getEvent(&humidity_event);
+
+    altitude = bme.readAltitude(SEALEVELPRESSURE_HPA);
+
+    // Accelerometer
+    //parameter accelGyro is the pointer to store the data
+    int rslt = bmi160.getAccelGyroData(accelGyro);
+
+    // Light sensor
+    lightReadings = readLightIntensity();
+  }
+
+  // Pre-flight. Runs checks to see when ascent begins. 
+  // [REQUIRES FINAL TWEAKS]
+  if(mode == 0){
+      Serial.println("\nPRE-FLIGHT PHASE");
+
+      // Multi-sensor data
+      Serial.print(F("Temperature = "));
+      Serial.print(temp_event.temperature);
+      Serial.println(" *C");
+      Serial.print(F("Humidity = "));
+      Serial.print(humidity_event.relative_humidity);
+      Serial.println(" %");
+      Serial.print(F("Pressure = "));
+      Serial.print(pressure_event.pressure);
+      Serial.println(" hPa");
+      Serial.print("Approx. Altitude = ");
+      Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+      Serial.println(" m");
+
+      // Accelerometer data
+      for(int i = 0; i < 6; i++){
+        if(i < 3) {
+          //the first three are gyro data
+          Serial.print(accelGyro[i]*3.14/180.0);Serial.print("\t");
+        }
+        else {
+          //the following three data are accel data
+          Serial.print(accelGyro[i]/16384.0);Serial.print("\t");
+        }
+      }
+      Serial.println();
+
+      if(multisensorWorks && altitude > 300){
+        Serial.println("Ascent detected using multi-sensor. Enabling atomizer.");
+        digitalWrite(A5, HIGH);
+        mode++;
+      }
+      else if(!multisensorWorks && accelerometerWorks && temporaryCounter > 200) {
+        Serial.println("Ascent detected using accelerometer. Enabling atomizer.");
+        digitalWrite(A5, HIGH);
+        mode++;
+      }
+      else{
+        Serial.println("Ascent not detected.");
+      }
+  }
+
+  // Ascent. Runs checks to see when descent begins.
+  // [REQUIRES VALVE IMPLEMENTATION]
+  else if(mode == 1){
+    Serial.println("\nASCENT PHASE");
+
+    if (multisensorWorks && altitude > highest_point){
+      highest_point = altitude;
+    }
+
+    if (multisensorWorks && altitude < highest_point - 20){
+      Serial.println("Descent detected using multi-sensor. Disabling atomizer.");
+      mode++;
+      digitalWrite(A5, LOW);
+      // open the valve (at the moment, i dont remember how to do so)
+    }
+    else if (!multisensorWorks && accelerometerWorks && temporaryCounter > 10){
+      Serial.println("Descent detected using accelerometer. Disabling atomizer.");
+      mode++;
+      digitalWrite(A5, LOW);
+      // open the valve (at the moment, i dont remember how to do so)
+    }
+    else {
+      Serial.println("Descent not detected.");
+    }
+  }  
+
+  // Descent. Runs checks for hitting the ground 
+  // [REQUIRES VALVE IMPLEMENTATION & BETTER DETECTION]
+  else if (mode == 2) {
+    Serial.println("\nDESCENT PHASE");
+
+    // i dont remember if the valve should eventually close here
+    if (accelerometerWorks & temporaryCounter < -20) {
+      Serial.println("End of descent detected using accelerometer. Enabling buzzer.");
+      mode++;
+      digitalWrite(buzzerPin, HIGH);
+    }
+    else{
+      Serial.println("End of descent not detected.");
+    }
+
+  }
+
+  // [LORA IMPLEMENTATION] (undecided yet)
+
+  // [SD IMPLEMENTATION]
+
+}
