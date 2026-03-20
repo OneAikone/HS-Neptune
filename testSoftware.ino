23532cfadfc03ed580b8266b3195ed5a193ff13f
OneAikone <aleksander.wiland10@gmail.com>
2026-03-20 23:55:42 +0100

    Enhance sensor integration and add SD card support
    Added support for additional sensors and SD card functionality.

--- testSoftware.ino
+++ testSoftware.ino
@@ -1,7 +1,8 @@
 #include <Wire.h>
 #include <SPI.h>
+#include <SD.h> // SD card (obvious)
 #include <Adafruit_BME280.h> // Multi-sensor (pressure, humidity, smth)
-#include <Adafruit_Sensor.h> // All adafruit
+#include <Adafruit_Sensor.h> // general adafruit library
 #include "Adafruit_TSL2591.h" // Light intensity
 #include <DFRobot_BMI160.h> // Accelerometer
 // connect SCL to I2C Clock
@@ -9,18 +10,38 @@
 // connect Vin to 3.3-5V DC
 // connect GROUND to common ground
 
-
 #define SEALEVELPRESSURE_HPA (1013.25)
 
+#define buzzerPin 7
+#define valvePin 8
+#define laserPin 9
+#define atomizerPin A1
+
+#define OutsideMultisensorAddress 0x77
+#define InnerMultisensorAddress 0x76
+
+#define SD_ChipSelect 10
 
 DFRobot_BMI160 bmi160; // Accelerometer
-const int8_t i2c_addr = 0x69; // I2C Address
+const int8_t i2c_addr = 0x69; // I2C Address for light sensor
 
 Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // Light intensity; pass in a number for the sensor identifier (for your use later)
-Adafruit_BME280 bme; // use I2C interface
-Adafruit_Sensor *bme_temp = bme.getTemperatureSensor();
-Adafruit_Sensor *bme_pressure = bme.getPressureSensor();
-Adafruit_Sensor *bme_humidity = bme.getHumiditySensor();
+Adafruit_BME280 bmeInside; // use I2C address 0x76
+Adafruit_Sensor *bme_temp1 = bmeInside.getTemperatureSensor();
+Adafruit_Sensor *bme_pressure1 = bmeInside.getPressureSensor();
+Adafruit_Sensor *bme_humidity1 = bmeInside.getHumiditySensor();
+
+Adafruit_BME280 bmeOutside; // use I2C address 0x77
+Adafruit_Sensor *bme_temp2 = bmeOutside.getTemperatureSensor();
+Adafruit_Sensor *bme_pressure2 = bmeOutside.getPressureSensor();
+Adafruit_Sensor *bme_humidity2 = bmeOutside.getHumiditySensor();
+
+int16_t accelGyro[6];
+
+uint16_t lightReadings[2] = {0,0};
+float lux = 0;
+
+File dataFile;
 
 void dispLightSensorDetails(void)
 {
@@ -55,23 +76,6 @@ void configureLightSensor(void)
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
   /* Display the gain and integration time for reference sake */  
   Serial.println(F("------------------------------------"));
-  Serial.print  (F("Gain:         "));
-  tsl2591Gain_t gain = tsl.getGain();
-  switch(gain)
-  {
-    case TSL2591_GAIN_LOW:
-      Serial.println(F("1x (Low)"));
-      break;
-    case TSL2591_GAIN_MED:
-      Serial.println(F("25x (Medium)"));
-      break;
-    case TSL2591_GAIN_HIGH:
-      Serial.println(F("428x (High)"));
-      break;
-    case TSL2591_GAIN_MAX:
-      Serial.println(F("9876x (Max)"));
-      break;
-  }
   Serial.print  (F("Timing:       "));
   Serial.print((tsl.getTiming() + 1) * 100, DEC);
   Serial.println(F(" ms"));
@@ -87,100 +91,167 @@ void readLightIntensity(void)
   uint16_t ir, full;
   ir = lum >> 16;
   full = lum & 0xFFFF;
-  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
+  //Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
   Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
   Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  "));
-  Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  "));
+  //Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  "));
   Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
+  lightReadings[0] = full;
+  lightReadings[1] = ir;
+  lux = tsl.calculateLux(full, ir);
 }
 
 void setup(void)
 {
-  Serial.begin(9600);
+ Serial.begin(9600);
 
-  pinMode(A5, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(valvePin, OUTPUT);
+  pinMode(laserPin, OUTPUT);
+  pinMode(atomizerPin, OUTPUT);
+
 
   // Light setup
   if (tsl.begin()) {
-    Serial.println(F("Found a TSL2591 sensor"));
+    Serial.println(F("TSL g"));
   }
   else {
-    Serial.println(F("No TSL2591 Light intensity sensor found ... check your wiring?"));
+    Serial.println(F("TSL not g"));
   }
+
   configureLightSensor();
-  dispLightSensorDetails();
 
   // Multi-sensor setup
-  if (!bme.begin(0x76)) {
-    Serial.println(F("Could not find a valid BME280 mult-sensor, check wiring!"));
+  if (!bmeInside.begin(0x76)) {
+    Serial.println(F("inner MultiSensor not g"));
   }
   else {
-    Serial.println(F("Found a BME280 sensor"));
+    Serial.println(F("inner BME280 g"));
   }
-  bme_temp->printSensorDetails();
-  bme_pressure->printSensorDetails();
-  bme_humidity->printSensorDetails();
+  bme_temp1->printSensorDetails();
+  bme_pressure1->printSensorDetails();
+  bme_humidity1->printSensorDetails();
+
+  if (!bmeOutside.begin(0x77)) {
+    Serial.println(F("outside MultiSensor not g"));
+  }
+  else {
+    Serial.println(F("Foutside BME280 g"));
+  }
+
+  bme_temp2->printSensorDetails();
+  bme_pressure2->printSensorDetails();
+  bme_humidity2->printSensorDetails();
+
 
   // Accelerometer setup
   if (bmi160.I2cInit(i2c_addr) != BMI160_OK){
-    Serial.println(F("Failed to initialise Accelerometer, check wiring ig gng"));
+    Serial.println(F("bmi160 not g ig"));
+  }
+
+  // SD card setup
+  if(SD.begin(SD_ChipSelect)) {
+    Serial.println(F("SD g"));
+  }
+  else {
+    Serial.println(F("SD fail oopsie :<"));
   }
 }
 
-// Flight stages; 0 -> pre-flight; 1 -> Ascent (atomizer enables); 2 -> Descent (valve opens, atomizer disables); 3 -> Landed (Valve closes, buzzer activates)
-int mode = 0;
-int temporaryCounter = 0;
-
 void loop(void)
 {
-  digitalWrite(A5, HIGH);
   delay(1000);
-  // Light intensity
-  readLightIntensity();
-
+  digitalWrite(atomizerPin, HIGH);
+  digitalWrite(laserPin, HIGH);
+  digitalWrite(valvePin, HIGH);
+  
   // Multi sensor
-  sensors_event_t temp_event, pressure_event, humidity_event;
-  bme_temp->getEvent(&temp_event);
-  bme_pressure->getEvent(&pressure_event);
-  bme_humidity->getEvent(&humidity_event);
- 
-  Serial.print(F("Temperature = "));
-  Serial.print(temp_event.temperature);
-  Serial.println(" *C");
+  sensors_event_t temp_event1, pressure_event1, humidity_event1, temp_event2, pressure_event2, humidity_event2;
 
-  Serial.print(F("Humidity = "));
-  Serial.print(humidity_event.relative_humidity);
-  Serial.println(" %");
+  bme_temp1->getEvent(&temp_event1);
+  bme_pressure1->getEvent(&pressure_event1);
+  bme_humidity1->getEvent(&humidity_event1);
 
-  Serial.print(F("Pressure = "));
-  Serial.print(pressure_event.pressure);
-  Serial.println(" hPa");
+  bme_temp2->getEvent(&temp_event2);
+  bme_pressure2->getEvent(&pressure_event2);
+  bme_humidity2->getEvent(&humidity_event2);
 
-  Serial.print("Approx. Altitude = ");
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
-  Serial.println(" m");
+  auto altitude1 = bmeInside.readAltitude(SEALEVELPRESSURE_HPA);
+  auto altitude2 = bmeOutside.readAltitude(SEALEVELPRESSURE_HPA);
 
   // Accelerometer
-  int i = 0;
-  int rslt;
-  int16_t accelGyro[6]={0};
- 
-  //get both accel and gyro data from bmi160
   //parameter accelGyro is the pointer to store the data
-  rslt = bmi160.getAccelGyroData(accelGyro);
-  if(rslt == 0){
-    for(i=0;i<6;i++){
-      if (i<3){
-        //the first three are gyro data
-        Serial.print(accelGyro[i]*3.14/180.0);Serial.print("\t");
-      }else{
-        //the following three data are accel data
-        Serial.print(accelGyro[i]/16384.0);Serial.print("\t");
-      }
+  int rslt = bmi160.getAccelGyroData(accelGyro);
+
+  // Light sensor
+  readLightIntensity();
+
+  Serial.println(F("\nPRE-FLIGHT"));
+
+  // Multi-sensor data
+  Serial.print(F("Temp Inside = "));
+  Serial.print(temp_event1.temperature);
+  Serial.println(" *C");
+  Serial.print(F("Hum Inside = "));
+  Serial.print(humidity_event1.relative_humidity);
+  Serial.println(" %");
+  Serial.print(F("Pres Inside = "));
+  Serial.print(pressure_event1.pressure);
+  Serial.println(" hPa");
+  Serial.print("Approx. Alt = ");
+  Serial.print(altitude1);
+  Serial.println(" m\n\n");
+
+  
+  Serial.print(F("Temp Outside = "));
+  Serial.print(temp_event2.temperature);
+  Serial.println(" *C");
+  Serial.print(F("Hum Outside = "));
+  Serial.print(humidity_event2.relative_humidity);
+  Serial.println(" %");
+  Serial.print(F("Pres Outside = "));
+  Serial.print(pressure_event2.pressure);
+  Serial.println(" hPa");
+  Serial.print("Approx. Alt = ");
+  Serial.print(altitude2);
+  Serial.println(" m");
+
+  // Accelerometer data
+  for(int i = 0; i < 6; i++){
+    if(i < 3) {
+      //the first three are gyro data
+      Serial.print(accelGyro[i]*3.14/180.0);Serial.print("\t");
     }
-    Serial.println();
-  }else{
-    Serial.println("err");
+    else {
+      //the following three data are accel data
+      Serial.print(accelGyro[i]/16384.0);Serial.print("\t");
+    }
   }
-  Serial.println("\n");
+  Serial.println();
+  dataFile = SD.open("CanSatDATA.csv", FILE_WRITE);
+  if (dataFile) {
+    dataFile.print(temp_event1.temperature); dataFile.print(F(", "));
+    dataFile.print(humidity_event1.relative_humidity); dataFile.print(F(", "));
+    dataFile.print(pressure_event1.pressure); dataFile.print(F(", "));
+    dataFile.print(temp_event2.temperature); dataFile.print(F(", "));
+    dataFile.print(humidity_event2.relative_humidity); dataFile.print(F(", "));
+    dataFile.print(pressure_event2.pressure); dataFile.print(F(", "));
+    dataFile.print(lightReadings[0]); dataFile.print(F(", "));
+    dataFile.print(lightReadings[1]); dataFile.print(F(", "));
+    dataFile.print(lux); dataFile.print(F(", "));
+    dataFile.print(accelGyro[0]); dataFile.print(F(", "));
+    dataFile.print(accelGyro[1]); dataFile.print(F(", "));
+    dataFile.print(accelGyro[2]); dataFile.print(F(", "));
+    dataFile.print(accelGyro[3]); dataFile.print(F(", "));
+    dataFile.print(accelGyro[4]); dataFile.print(F(", "));
+    dataFile.print(accelGyro[5]); dataFile.print(F(", "));
+    Serial.println(F("Saved data to SD."));
+    dataFile.close();
+  }
+  else{
+    Serial.println(F("SD save failed."));
+  }
+
+
+  // [LORA IMPLEMENTATION NEEDED] (probably sx1276)
 }
